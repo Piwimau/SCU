84afed90abffd53546c14020adccbf0e2dfbb4b3
Philipp Raschka <162504898+Piwimau@users.noreply.github.com>
2025-10-10 14:30:08 +0200

    Add module for math functionality.

---
+++ include/scu/math.h
@@ -0,0 +1,28 @@
+#ifndef SCU_MATH_H
+#define SCU_MATH_H
+
+/**
+ * @brief Returns the minimum of two values.
+ *
+ * @warning Do not pass expressions with side effects as arguments, as they may
+ * be evaluated more than once.
+ *
+ * @param[in] a The first value.
+ * @param[in] b The second value.
+ * @return The minimum of `a` and `b`.
+ */
+#define SCU_MIN(a, b) (((a) < (b)) ? (a) : (b))
+
+/**
+ * @brief Returns the maximum of two values.
+ *
+ * @warning Do not pass expressions with side effects as arguments, as they may
+ * be evaluated more than once.
+ *
+ * @param[in] a The first value.
+ * @param[in] b The second value.
+ * @return The maximum of `a` and `b`.
+ */
+#define SCU_MAX(a, b) (((a) > (b)) ? (a) : (b))
+
+#endif
