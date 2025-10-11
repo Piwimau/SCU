e0a667520ab2cf93edff4b95d62a8c3e9e1bc9c8
Philipp Raschka <162504898+Piwimau@users.noreply.github.com>
2025-10-11 14:21:33 +0200

    Add main header.

---
+++ include/scu/scu.h
@@ -0,0 +1,26 @@
+#ifndef SCU_H
+#define SCU_H
+
+#include "scu/alloc.h"
+#include "scu/assert.h"
+#include "scu/common.h"
+#include "scu/error.h"
+#include "scu/io.h"
+#include "scu/math.h"
+#include "scu/memory.h"
+#include "scu/string.h"
+#include "scu/timer.h"
+
+/** @brief The major version number of SCU. */
+#define SCU_VERSION_MAJOR 0
+
+/** @brief The minor version number of SCU. */
+#define SCU_VERSION_MINOR 1
+
+/** @brief The patch version number of SCU. */
+#define SCU_VERSION_PATCH 0
+
+/** @brief The version string of SCU. */
+#define SCU_VERSION_STRING "0.1.0"
+
+#endif
