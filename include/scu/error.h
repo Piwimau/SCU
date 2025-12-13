8a8c1dd89c558a65164ed1368ed14ce0b392c6bf
Philipp Raschka <162504898+Piwimau@users.noreply.github.com>
2025-12-13 15:24:23 +0100

    Add new error constant.

--- include/scu/error.h
+++ include/scu/error.h
@@ -55,7 +55,10 @@ enum SCUError {
     SCU_ERROR_STOPWATCH_FAILED,
 
     /** @brief Indicates that an element is already present in a collection. */
-    SCU_ERROR_ELEM_PRESENT
+    SCU_ERROR_ELEM_PRESENT,
+
+    /** @brief Indicates that an input was not in the expected format. */
+    SCU_ERROR_INVALID_FORMAT
 
 };
 
